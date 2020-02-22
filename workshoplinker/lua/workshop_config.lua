5c4204053520ded37f2a9f1b64515e1f38f16a58
Dmitry <opensuse12@gmail.com>
2020-02-22 09:12:18 +0300

    Update workshop_config.lua

--- workshoplinker/lua/workshop_config.lua
+++ workshoplinker/lua/workshop_config.lua
@@ -1,2 +1,2 @@
 -- Workshop collection ID (Example link: http://steamcommunity.com/workshop/filedetails/?id=416357984) => 416357984
-CollectionID = "416357984";
+CollectionID = "your_workshop_collection_id";
